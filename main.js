538618067f6b848a848c4d7709697e8355509ab9
Kyler <Krenneker16@gmail.com>
2019-01-30 22:19:13 -0500

    added beyond function

--- main.js
+++ main.js
@@ -5,4 +5,38 @@ function jediName (firstName, lastName) {
     return `${first3OfLastName}${firstOfFirstName}`;
 }
 
+//test code
 console.log(jediName('Kyler', 'Renneker'));
+//end of test code
+
+
+
+function beyond (num) {
+
+    let response = '';
+
+    switch (true){
+        case (num === +Infinity || num === -Infinity):
+            response = 'And beyond';
+            break;
+        
+        case (num > 0 && Number.isFinite(num)):
+            response = 'To infinity';
+            break;
+
+        case (num < 0 && Number.isFinite(num)):
+            response = 'To negative infinity';
+            break;
+        case (num === 0):
+            response = 'Staying home';    
+    }
+
+    return response;
+}
+
+console.log(beyond(55));
+console.log(beyond(Infinity));
+console.log(beyond(-Infinity));
+console.log(beyond(0));
+console.log(beyond(-23));
+
