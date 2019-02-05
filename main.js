2c5d70bdf6c450f67261c76578d84418ebed98e7
Mengqi Wang <wmq516@gmail.com>
2019-02-04 19:16:37 -0700

    added daysInMonth solution

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//First two problems done by Kyler Renneker
 function jediName (firstName, lastName) {
     let first3OfLastName = lastName.slice(0, 3);
     let firstOfFirstName = firstName.slice(0, 2);
@@ -40,3 +41,87 @@ console.log(beyond(-Infinity));
 console.log(beyond(0));
 console.log(beyond(-23));
 
+//Cracking the code
+
+function decode(string) {
+    let arr = string.split(" ");
+    //console.log(arr)
+    let newArr = [];
+    //console.log(newArr);
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i][0] == 'a') {
+            newArr.push(arr[i][1]);
+        } else if (arr[i][0] == 'b') {
+            //console.log('in b block')
+            newArr.push(arr[i][2]);
+           //console.log(newArr)
+        } else if (arr[i][0] == 'c') {
+            //console.log('in c block');
+            newArr.push(arr[i][3]);
+            //console.log(newArr)
+        } else if (arr[i][0] == 'd') {
+            newArr.push(arr[i][4]);
+        } else {newArr.push(" ");}
+    }
+    return newArr;
+}
+
+let testString = "block argon meter bells brown croon droop"
+
+// Below is 'How many days in a month'
+
+function daysInMonth(month, leapYear) {
+    switch(leapYear) {
+        case true:
+        switch(month) {
+            case "January":
+            case "March":
+            case "May":
+            case "July":
+            case "August":
+            case "October":
+            case "December":
+                console.log(`${month} has 31 days.`)
+                break;
+            case "February":
+                console.log(`${month} has 29 days.`)
+                break;
+            case "April":
+            case "June":
+            case "September":
+            case "November":
+                console.log(`${month} has 30 days.`)
+                break;
+        }
+    }
+    switch(leapYear) {
+        case false:
+        switch(month) {
+            case "January":
+            case "March":
+            case "May":
+            case "July":
+            case "August":
+            case "October":
+            case "December":
+                console.log(`${month} has 31 days.`)
+                break;
+            case "February":
+                console.log(`${month} has 28 days.`)
+                break;
+            case "April":
+            case "June":
+            case "September":
+            case "November":
+                console.log(`${month} has 30 days.`)
+                break;
+        }
+    }
+}
+
+//Test codes
+daysInMonth('February', true);
+daysInMonth('February', false);
+daysInMonth('March', false);
+daysInMonth('November', true);
+//End of test codes
